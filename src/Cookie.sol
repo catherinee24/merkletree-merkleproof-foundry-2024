c92022b48f4a701a37557dd70804987faceb537c
catellaTech <catellatech@gmail.com>
2024-08-23 13:48:58 -0500

    Merkle Tree & Merkle Proof: foundry 2024

--- src/Cookie.sol
+++ src/Cookie.sol
@@ -5,6 +5,10 @@ pragma solidity ^0.8.24;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * Author: Catellatech
+ */
+
 contract Cookie is ERC20 , Ownable { 
     constructor() ERC20("Cookie", "COOK") Ownable(msg.sender) {}
 
